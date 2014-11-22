b2a3d639f32b8ba6748cfbe917b4e33bd47ee9ed
Dexter89 <simone.bosc@gmail.com>
2014-11-22 15:27:54 +0100

    Added ILaserCatHardwae interface and moved related struct declaration together

---
+++ bitLab.LaserCat.Grbl/ILaserCatHardware.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bitLab.LaserCat.Grbl
+{
+  // Stores the planner block Bresenham algorithm execution data for the segments in the segment 
+  // buffer. Normally, this buffer is partially in-use, but, for the worst case scenario, it will
+  // never exceed the number of accessible stepper buffer segments (SEGMENT_BUFFER_SIZE-1).
+  // NOTE: This data is copied from the prepped planner blocks so that the planner blocks may be
+  // discarded when entirely consumed and completed by the segment buffer. Also, AMASS alters this
+  // data for its own use. 
+  public struct st_block_t
+  {
+    public byte direction_bits;
+    public uint[] steps;
+    public uint step_event_count;
+
+    public st_block_t(bool dummy)
+    {
+      direction_bits = 0;
+      steps = new uint[NutsAndBolts.N_AXIS];
+      step_event_count = 0;
+    }
+  };
+
+  // Primary stepper segment ring buffer. Contains small, short line segments for the stepper 
+  // algorithm to execute, which are "checked-out" incrementally from the first block in the
+  // planner buffer. Once "checked-out", the steps in the segments buffer cannot be modified by 
+  // the planner, where the remaining planner block steps still can.
+  public struct segment_t
+  {
+    public short n_step;          // Number of step events to be executed for this segment
+    public byte st_block_index;   // Stepper block data index. Uses this information to execute this segment.
+    public ushort cycles_per_tick; // Step distance traveled per ISR tick, aka step rate.  
+    public byte amass_level;    // Indicates AMASS level for the ISR to execute this segment  
+    public byte prescaler;      // Without AMASS, a prescaler is required to adjust for slow timing.
+  };
+
+  public interface ILaserCatHardware
+  {
+    void StorePlannerBlock(byte blockIndex, st_block_t block);
+    void StoreSegment(byte segmentIndex, segment_t segment);
+  }
+}
--- bitLab.LaserCat.Grbl/Stepper.cs
+++ bitLab.LaserCat.Grbl/Stepper.cs
@@ -3,26 +3,6 @@ namespace bitLab.LaserCat.Grbl
 {
   unsafe public partial class GrblFirmware
   {
-    // Stores the planner block Bresenham algorithm execution data for the segments in the segment 
-    // buffer. Normally, this buffer is partially in-use, but, for the worst case scenario, it will
-    // never exceed the number of accessible stepper buffer segments (SEGMENT_BUFFER_SIZE-1).
-    // NOTE: This data is copied from the prepped planner blocks so that the planner blocks may be
-    // discarded when entirely consumed and completed by the segment buffer. Also, AMASS alters this
-    // data for its own use. 
-    public struct st_block_t
-    {
-      public byte direction_bits;
-      public uint[] steps;
-      public uint step_event_count;
-
-      public st_block_t(bool dummy)
-      {
-        direction_bits = 0;
-        steps = new uint[NutsAndBolts.N_AXIS];
-        step_event_count = 0;
-      }
-    };
-
     private static st_block_t[] CreateStBlockArray(int count)
     {
       var array = new st_block_t[count];
@@ -41,19 +21,6 @@ namespace bitLab.LaserCat.Grbl
       if (StepperSegmentBufferChanged != null)
         StepperSegmentBufferChanged(this, EventArgs.Empty);
     }
-
-    // Primary stepper segment ring buffer. Contains small, short line segments for the stepper 
-    // algorithm to execute, which are "checked-out" incrementally from the first block in the
-    // planner buffer. Once "checked-out", the steps in the segments buffer cannot be modified by 
-    // the planner, where the remaining planner block steps still can.
-    public struct segment_t
-    {
-      public short n_step;          // Number of step events to be executed for this segment
-      public byte st_block_index;   // Stepper block data index. Uses this information to execute this segment.
-      public ushort cycles_per_tick; // Step distance traveled per ISR tick, aka step rate.  
-      public byte amass_level;    // Indicates AMASS level for the ISR to execute this segment  
-      public byte prescaler;      // Without AMASS, a prescaler is required to adjust for slow timing.
-    };
 
     public segment_t[] segment_buffer = new segment_t[SEGMENT_BUFFER_SIZE];
 
