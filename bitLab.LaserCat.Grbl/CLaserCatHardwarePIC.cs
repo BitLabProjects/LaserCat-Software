7812e7d9ae2e013c3bd388428b0b79dc7a219532
andrea_borin <andrea.borin00@gmail.com>
2014-11-30 17:34:46 +0100

    Started implementation of PIC serial communication

---
+++ bitLab.LaserCat.Grbl/CLaserCatHardwarePIC.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace bitLab.LaserCat.Grbl
+{
+	class CLaserCatHardwarePIC : ILaserCatHardware
+	{
+		private SerialPort mSerialPort;
+		private String mPortName;
+
+		private Queue<byte> mReceiveBuffer;
+
+		private const char START_CHAR = '§';
+		private enum EReadingState { WaitingForStartChar, WaitingForReadLength, Reading };
+		private EReadingState mReadingState;
+		private int mCharToRead;
+
+		public event EventHandler CommandParsed;
+		private void RaiseCommandParsed()
+		{
+			if (CommandParsed != null)
+				CommandParsed(this, EventArgs.Empty);
+		}
+
+		CLaserCatHardwarePIC(string portName)
+		{
+			mReadingState = EReadingState.WaitingForStartChar;
+			mCharToRead = 0;
+			mPortName = portName;
+			mReceiveBuffer = new Queue<byte>();
+			mSerialPort = new SerialPort(mPortName);
+			mSerialPort.BaudRate = 115200;
+			mSerialPort.Parity = Parity.None;
+			mSerialPort.DataBits = 8;
+			mSerialPort.StopBits = StopBits.One;
+			mSerialPort.DataReceived += ReceiveFromPIC;
+		}
+
+		public void Init()
+		{
+			//TODO
+			SendToPIC("Init");
+		}
+
+		public void Reset()
+		{
+			//TODO
+			SendToPIC("Reset");
+		}
+
+		public void SetSettings(LaserCatSettings settings)
+		{
+			//TODO
+			SendToPIC("Init");
+		}
+
+		public void WakeUp(bool setupAndEnableMotors)
+		{
+			//TODO
+			SendToPIC("WakeUp," + Convert.ToString(setupAndEnableMotors));
+		}
+
+		public void GoIdle(bool delayAndDisableSteppers)
+		{
+			//TODO
+			SendToPIC("GoIdle," + Convert.ToString(delayAndDisableSteppers));
+		}
+
+		public void StorePlannerBlock(byte blockIndex, st_block_t block)
+		{
+			//TODO
+			SendToPIC(string.Format("StoreBlock,{0},{1},{2},{3}",
+															blockIndex,
+															Convert.ToString(block.direction_bits),
+															Convert.ToString(block.step_event_count),
+															Convert.ToString(block.steps)));
+		}
+
+		public bool GetHasMoreSegmentBuffer()
+		{
+			//TODO
+			return true;			
+		}
+
+		public void StoreSegment(segment_t segment)
+		{
+			//TODO
+			SendToPIC(string.Format("StoreSegment,{0},{1},{2},{3},{4}",
+															Convert.ToString(segment.amass_level),
+															Convert.ToString(segment.cycles_per_tick),
+															Convert.ToString(segment.n_step),
+															Convert.ToString(segment.prescaler),
+															Convert.ToString(segment.st_block_index)));
+		}
+
+		private void SendToPIC(String data)
+		{
+			mSerialPort.Write(FormatCommandForSend(data));
+		}
+
+		private String FormatCommandForSend(String data)
+		{
+			String formattedData = Convert.ToString(START_CHAR);
+			int dataLength = data.Length;
+			formattedData += Convert.ToChar(dataLength);
+			formattedData += data;
+			return formattedData;
+		}
+
+		private void ReceiveFromPIC(object sender, SerialDataReceivedEventArgs e)
+		{
+			SerialPort sp = (SerialPort)sender;
+			int bufferLength = sp.BytesToRead;
+			byte[] buffer = new byte[255];
+			sp.Read(buffer, 0, bufferLength);
+
+			int i;
+			for (i = 0; i < bufferLength; i++)
+			{
+				char currChar = Convert.ToChar(buffer[i]);
+
+				switch (mReadingState)
+				{
+					case EReadingState.WaitingForStartChar:
+						{
+							if (currChar == START_CHAR) mReadingState = EReadingState.WaitingForReadLength;
+						}
+						break;
+					case EReadingState.WaitingForReadLength:
+						{
+							mCharToRead = Convert.ToInt32(currChar);
+						}
+						break;
+					case EReadingState.Reading:
+						{
+							if (mCharToRead == 0)
+							{
+								mReadingState = EReadingState.WaitingForStartChar;
+								ParseCommand();
+								mReceiveBuffer = new Queue<byte>();
+							}
+							else
+							{
+								mReceiveBuffer.Enqueue(buffer[i]);
+								mCharToRead--;
+							}
+						}
+						break;
+				}
+			}
+		}
+
+		private void ParseCommand()
+		{
+			//TODO
+			RaiseCommandParsed();
+		}
+
+	}
+}
